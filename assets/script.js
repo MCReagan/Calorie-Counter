df03ad7e9ac8a6f2068228d8b5ad9fabc17b2025
Sam <samuelhalloum@gmail.com>
2023-04-17 12:55:06 -0500

    d

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
-var query = 'italian wedding soup'
+
+function ingredientList(food){
+    var foodIngredients = []
+    // returns a list of ingredients in a recipe
 $.ajax({
     method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/recipe?query=' + query,
-    headers: { 'X-Api-Key': 'DFSgX/7pfugOaW/IOAGavw==KeyRO8WYPbJ5bGoZ'},
+    url: 'https://api.api-ninjas.com/v1/recipe?query=' + food,
+    headers: { 'X-Api-Key': '3TpEafSFnQPCwY3sTujznK9xeBtbG98f8IMZ7H44'},
     contentType: 'application/json',
     success: function(result) {
-        var foodIngredients = result[1].ingredients
-        console.log(foodIngredients);
-        console.log(result)
+        for (var food of result[1].ingredients.split('|')){
+            foodIngredients.push(food)
+        }
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
+    
 });
+return foodIngredients
+}
+
+console.log(ingredientList('italian wedding soup'))
 
 
-
-// logs ingredients in variable foodIngredients
