1fd1230ed0966a9484d0c966d4846b23563b77b0
Sam <samuelhalloum@gmail.com>
2023-04-18 13:03:48 -0500

    linked APIs to get ingredients to recipe

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,40 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '3TpEafSFnQPCwY3sTujznK9xeBtbG98f8IMZ7H44',
-		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-	}
-};
 
-$("#search-btn").on("click", function () {
-    var query = $("#search-input").val().trim();
-    localStorage.setItem('recipe', query);
-});
+function getIngredientsFromId(query){
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'a0cc7ba7dcmshb8b57f3adb29db3p11639djsnd89090b0b1ea',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/' + query + '/information', options)
+    .then(response => response.json())
+    .then(response => console.log(response.extendedIngredients))
+    .catch(err => console.error(err));
+}
 
 
-// () =======
-// logs ingredients in variable foodIngredients
+function getRecipeID(query){
+  var recipeId = ''
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'a0cc7ba7dcmshb8b57f3adb29db3p11639djsnd89090b0b1ea',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  };
+    
+  fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=' + query, options)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+      console.log(data)
+      console.log(data.results[Math.floor(Math.random() * 10)].id)
+      getIngredientsFromId(String(data.results[Math.floor(Math.random() * 10)].id))
+    })
+}
+
+
+getRecipeID('tiramisu')
