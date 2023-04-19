26ac7119a3f86db8165fbf45416e4f5da2bfcc37
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-19 10:36:55 -0500

    merging

--- assets/script.js
+++ assets/script.js
@@ -5,77 +5,6 @@
 // contentType: "application/json", ======= is the data that is sent to the server
 // success: function (result) ======= calling back for the successful response
 // error: function ajaxError(jqXHR) ======= call back for the error response that takes the (jqXHR) object as a argument and logs errors to the console.
-<<<<<<< HEAD
-$(document).ready(function () {
-  var storedQuery = localStorage.getItem("lastSearchQuery");
-  if (storedQuery) {
-    searchInput.val(storedQuery);
-  }
-});
-var searchBtn = $("#search-btn");
-var searchInput = $("#search-input");
-var homeBtn = $("#home-button");
-var errorMessage = $("#error-message");
-
-$("#search-btn").on("click", function () {
-  var query = $("#search-input").val().trim();
-
-  if (query) {
-    searchRecipes(query);
-    // localStorage.setItem('recipe', query);
-    // recipes.push(query);
-    // // displayRecipe();
-    // window.location.href = ("results.html");
-  } else {
-    displayError("this is a error message");
-  }
-});
-
-searchInput.on("keydown", function (event) {
-  if (event.key === "Enter") {
-    var query = searchInput.val().trim();
-    if (query) {
-      searchRecipes(query);
-    } else {
-      displayError("this is a error message");
-    }
-  }
-});
-
-homeBtn.on("click", function () {
-  window.location.href = "index.html";
-});
-
-// $("#home-button").on("click", function () {
-//   window.location.href = "index.html";
-// };)
-// var recipes = [];
-// var query = "italian wedding soup";
-
-function searchRecipes(query) {
-  console.log('Searching for recipes with "${query}"');
-  localStorage.setIem("lastSearchQuery", query);
-
-  $.ajax({
-    method: "GET",
-    url: "https://api.api-ninjas.com/v1/recipe?query=" + query,
-    headers: { "X-Api-Key": "DFSgX/7pfugOaW/IOAGavw==KeyRO8WYPbJ5bGoZ" },
-    contentType: "application/json",
-    success: function (result) {
-      displayRecipe(result);
-      // var foodIngredients = result[1].ingredients;
-      // console.log(foodIngredients);
-      // console.log(result);
-    },
-
-    error: function ajaxError(jqXHR) {
-      console.error("Error: ", jqXHR.responseText);
-    },
-  });
-}
-
-=======
->>>>>>> 
 
 
 // function displayRecipes(recipes) ======= function to display the fetched recipes for UI with that it takes the selected array as the arguments (forEach())
@@ -114,8 +43,6 @@ function hideError() {
 // $("#search-btn").on("click", function () ======= the event Listener for the search button.
 // var query = $("#search-input").val().trim(); ======= stores the value for the search input field
 // fetchRecipes(query); ======= calls the "(fetchRecipes())" function via query as a argument
-<<<<<<< HEAD
-=======
 $(document).ready(function () {
 var storedQuery = localStorage.getItem("lastSearchQuery");
 if (storedQuery) {
@@ -170,7 +97,6 @@ homeBtn.on("click", function() {
 $("#home-button").on('click', function () {
 window.location.href = ("index.html");
 });
->>>>>>> 5bff151a576f0e15f54ceb7ed974debc855fbce3
 
 
 function getIngredientsFromId(query){
